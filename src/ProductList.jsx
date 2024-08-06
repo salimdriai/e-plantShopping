27f15e3702771f1e444996709c3c1943e713c4ac
salimdriai <salimdriai15@gmail.com>
2024-08-06 14:16:18 +0100

    center the title

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -368,9 +368,15 @@ function ProductList() {
         <div className="product-grid">
           {plantsArray.map((category, index) => (
             <div key={index}>
-              <h1>
-                <div>{category.category}</div>
+              <h1
+                style={{
+                  textAlign: "center",
+                  paddingTop: 16,
+                }}
+              >
+                {category.category}
               </h1>
+
               <div className="product-list">
                 {category.plants.map((plant, plantIndex) => (
                   <div className="product-card" key={plantIndex}>
